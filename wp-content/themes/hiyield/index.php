650bb0b64359c25596faacff6b598806ea79ffdb
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-04-18 20:02:30 +0100

    Add new theme, hiyield

---
+++ wp-content/themes/hiyield/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mx-auto flex flex-wrap justify-center gap-8 p-4">
+
+        <div class="w-auto h-auto">
+
+            <div class="rounded-md overflow-hidden">
+                <img src="https://picsum.photos/id/232/400/200" alt="a random image" class="object-cover">
+            </div>
+
+            <p class="font-semibold">Name here</p>
+            <a href="#" class="text-red-600 text-sm">View Portfolio</a>
+
+        </div>
+
+        <div class="w-auto h-auto">
+
+            <div class="rounded-md overflow-hidden">
+                <img src="https://picsum.photos/id/231/400/200" alt="a random image" class="object-cover">
+            </div>
+
+            <p class="font-semibold">Name here</p>
+            <a href="#" class="text-red-600 text-sm">View Portfolio</a>
+
+        </div>
+
+        <div class="w-auto h-auto">
+
+            <div class="rounded-md overflow-hidden">
+                <img src="https://picsum.photos/id/213/400/200" alt="a random image" class="object-cover">
+            </div>
+
+            <p class="font-semibold">Name here</p>
+            <a href="#" class="text-red-600 text-sm">View Portfolio</a>
+
+        </div>
+
+        <div class="w-auto h-auto">
+
+            <div class="rounded-md overflow-hidden">
+                <img src="https://picsum.photos/id/124/400/200" alt="a random image" class="object-cover">
+            </div>
+
+            <p class="font-semibold">Name here</p>
+            <a href="#" class="text-red-600 text-sm">View Portfolio</a>
+
+        </div>
+
+        <div class="w-auto h-auto">
+
+            <div class="rounded-md overflow-hidden">
+                <img src="https://picsum.photos/id/244/400/200" alt="a random image" class="object-cover">
+            </div>
+
+            <p class="font-semibold">Name here</p>
+            <a href="#" class="text-red-600 text-sm">View Portfolio</a>
+
+        </div>
+
+ 
+
+    </div>
+    
+</body>
+</html>
